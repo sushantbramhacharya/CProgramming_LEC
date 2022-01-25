a16873a0ac53023bcacdafbfb02ce5cf3b93d20f
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-01-25 09:42:15 +0545

    Arthmetic Operator

---
+++ Chapter3_CBasics/Operator_and_expression/arthematic_operator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int a=21;
+    int b=10;
+    int c;
+
+    c=a+b;
+    printf("Line 1 - Value of c is %d \n",c);
+
+    c=a-b;
+    printf("Line 2 - Value of c is %d\n",c);
+
+    c=a*b;
+    printf("Line 3 - Value of c is %d\n",c);
+
+    c=a/b;
+    printf("Line 4 - Value of c is %d\n",c);
+
+    c=a%b;
+    printf("Line 5 - Value of c is %d\n",c);
+
+    c=a++;
+    printf("Line 7 - Value of c is %d\n",c);
+    
+    c=++a;
+    printf("Line 8 - Value of c is %d\n",c);
+
+    c=a--;
+    printf("Line 9 - Value of c is %d\n",c);
+}
