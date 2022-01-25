3475d3cb8b49b9fa3ef8a0f1679787a6e88d6493
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-01-25 10:05:35 +0545

    Assignment Operator

---
+++ Chapter3_CBasics/Operator_and_expression/assignment_operator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() {
+   int a = 21;
+   int c ;
+
+   c =  a;
+   printf("Line 1 - =  Value of c = %d\n", c );
+
+   c +=  a;
+   printf("Line 2 - += Value of c = %d\n", c );
+
+   c -=  a;
+   printf("Line 3 - -= Value of c = %d\n", c );
+
+   c *=  a;
+   printf("Line 4 - *= Value of c = %d\n", c );
+
+   c /=  a;
+   printf("Line 5 - /= Value of c = %d\n", c );
+
+   c  = 200;
+   c %=  a;
+   printf("Line 6 - %=  Value of c = %d\n", c );
+}
