dc24ecfb9087caafbe45d222632b97b3267dd25e
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-02-01 09:53:51 +0545

    switch case conditional statement

---
+++ Chapter3_CBasics/Condition/switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    char grade;
+    printf("Enter your grade : \n");
+    scanf("%c", &grade);
+
+    switch (grade)
+    {
+    case 'A':
+        printf("Excellent\n");
+        break;
+    case 'B':
+    case 'C':
+        printf("Well done\n");
+        break;
+    case 'D':
+        printf("You passed\n");
+        break;
+    case 'F':
+        printf("Better try again\n");
+    default:
+        printf("Invald Grade\n");
+        break;
+    }
+    printf("Your grade is %c", grade);
+}
