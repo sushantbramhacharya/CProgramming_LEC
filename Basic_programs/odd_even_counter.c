5396478a88fb936cb4209166f29933ed2b1e5031
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-03 12:57:46 +0545

    odd even Counter

---
+++ Basic_programs/odd_even_counter.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num=1,count_odd=0,count_even=0;
+    while(num!=0)
+    {
+    printf("Enter Numbers and enter 0 to cancel:\n");
+    scanf("%d",&num);
+    if(num%2==0)
+        count_even++;
+    else
+        count_odd++;
+    }
+    printf("Count of odd is %d and Even is %d",count_odd,count_even);
+}
