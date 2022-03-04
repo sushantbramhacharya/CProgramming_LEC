3a78bdf4ab297d3fc291a9516e604c08729b20ef
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-04 20:51:21 +0545

    Reverse of any number using loop

---
+++ Basic_programs/Reverse_any_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int num,rem,reverse=0;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    while(num)
+    {
+        rem=num%10;
+        reverse=reverse*10+rem;
+        num=num/10;
+    }
+    printf("Reverse is %d",reverse);
+}
