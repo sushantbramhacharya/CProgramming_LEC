ccaed9952c3ab2d9bf86a8e65ff768c32216ea7c
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-05 09:19:44 +0545

    sum of the numbers exactly divisible by 7 not 5

---
+++ Chapter5_array/count.c
@@ -0,0 +1,25 @@
+//wap to count and find the sum of all number in the array which are exactly divided by 7 but not by 5
+#include<stdio.h>
+int sum(int array[5])
+{
+    int sum=0;
+    for(int i=0;i<5;i++)
+    {
+        if(array[i]%7==0&&array[i]%5!=0)
+        {
+            sum=sum+array[i];
+        }
+    }
+    return sum;
+}
+void main()
+{
+    int array[5],sumOfNumbers;
+    printf("Enter the numbers : ");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    sumOfNumbers=sum(array);
+    printf("The sum is %d",sumOfNumbers);
+}
