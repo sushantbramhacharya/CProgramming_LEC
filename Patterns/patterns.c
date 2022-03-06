14ad4c2b77fc4ebc65f987541bcd21bbd87f5115
Guilherme Capucci <gscapucci@hotmail.com>
2022-03-06 03:33:20 -0300

    separeate files in .h and .c

--- Patterns/patterns.c
+++ Patterns/patterns.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-void multiplicationTable();
-void floydTriangle();
-void triangularStar();
-void incrementNumbersTriangleLeft();
-void incrementNumbersTriangleRight(); // only two loops
-void chessPattern();
-void triangularStarRight();
-void incrementNumbersInvertTriangleRight(); // uses three loops
-void decrementNumbersInvertTriangleRight(); // uses three loops
-void decrementNumbersTriangleRight();       // only two loops
-void incrementDecrementPatternNumber();
-void squareStarPattern();
-void rectangleStarPattern();
-void pyramidPatternOne();
-void diamondPatternOne();
-void diamondPatternTwo();
-void plusPattern();
+
 void main()
 {
     plusPattern();
---
+++ Patterns/patterns.h
@@ -0,0 +1,22 @@
+#ifndef PATTERNS_H
+#define PATTERNS_H
+
+void multiplicationTable();
+void floydTriangle();
+void triangularStar();
+void incrementNumbersTriangleLeft();
+void incrementNumbersTriangleRight(); // only two loops
+void chessPattern();
+void triangularStarRight();
+void incrementNumbersInvertTriangleRight(); // uses three loops
+void decrementNumbersInvertTriangleRight(); // uses three loops
+void decrementNumbersTriangleRight();       // only two loops
+void incrementDecrementPatternNumber();
+void squareStarPattern();
+void rectangleStarPattern();
+void pyramidPatternOne();
+void diamondPatternOne();
+void diamondPatternTwo();
+void plusPattern();
+
+#endif /* PATTERNS_H */
