d08073d7298377d7cf45fb952de65ff2d27531c6
Guilherme Silva Capucci <50434328+gscapucci@users.noreply.github.com>
2022-03-06 02:47:32 -0300

    Add spaces for readability

--- Basic_programs/odd_even_counter.c
+++ Basic_programs/odd_even_counter.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int num=1,count_odd=0,count_even=0;
-    while(num!=0)
+    int num = 1, count_odd = 0,count_even = 0;
+    while(num != 0)
     {
     printf("Enter Numbers and enter 0 to cancel:\n");
-    scanf("%d",&num);
-    if(num%2==0)
+    scanf("%d", &num);
+    if(num % 2 == 0)
         count_even++;
     else
         count_odd++;
