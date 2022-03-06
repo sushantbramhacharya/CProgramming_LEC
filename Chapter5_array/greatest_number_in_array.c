9b93ca9f43a68c29f9b07531fe6237252c500a0a
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-06 14:56:52 +0545

    Greatest in an array

--- Chapter5_array/greatest_number_in_array.c
+++ Chapter5_array/greatest_number_in_array.c
@@ -1,14 +1,15 @@
+// Greatest Number in an array Linear Search Method
 #include <stdio.h>
 void main()
 {
     int array[10] = {3, 2, 1, 4, 4, 5, 6, 3, 7, 4};
-    int n = 10,greatest=0;
-    for(int i=0;i<n;i++)
+    int n = 10, greatest = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(greatest<array[i])
+        if (greatest < array[i])
         {
-            greatest=array[i];
+            greatest = array[i];
         }
     }
-    printf("The Greatest Number in an array is %d",greatest);
+    printf("The Greatest Number in an array is %d", greatest);
 }
