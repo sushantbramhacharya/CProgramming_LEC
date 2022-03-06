894bbd24f7802400a8aecf846727f1a734bf9ff1
Guilherme Silva Capucci <50434328+gscapucci@users.noreply.github.com>
2022-03-06 02:35:24 -0300

    Optmization
    A number will never be divisible by more then a half of itself

--- Basic_programs/Prime_or_not.c
+++ Basic_programs/Prime_or_not.c
@@ -4,14 +4,14 @@ void main()
     int num, i;
     printf("Enter a number : ");
     scanf("%d", &num);
-    for (i = 2; i < num; i++)
+    for (i = 2; i <= num / 2; i++)
     {
         if (num % i == 0)
         {
             break;
         }
     }
-    if (i == num)
+    if (i == num / 2 + 1)
         printf("Its Prime");
     else
         printf("Its not prime");
