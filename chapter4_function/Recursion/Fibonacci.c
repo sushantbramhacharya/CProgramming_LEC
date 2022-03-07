aaf62d66c5e5d459fe7462d0ecf04fcc20490fb0
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-07 18:01:13 +0545

    Fibonacci using recursion

---
+++ chapter4_function/Recursion/Fibonacci.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fibonacci(int i){ 
+	if(i==0) 
+        return 0; 
+	else if(i==1) 
+        return 1; 
+	else 
+        return (fibonacci(i-1)+fibonacci(i-2));
+        // 0 1 1 2 3 5 8
+} 
+void main()
+{
+    for(int i=1;i<10;i++)
+    {
+        printf("%d\t",fibonacci(i));
+    }
+    
+}
