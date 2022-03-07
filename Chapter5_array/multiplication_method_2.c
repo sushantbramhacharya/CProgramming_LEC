ba18225b43320d50f92ce06faa45b5cf2368736c
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-07 14:40:57 +0545

    Matrix Multiplication method 2

---
+++ Chapter5_array/multiplication_method_2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    int a[2][2],b[2][2],sum[2][2];
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            printf("Enter the number of %d i %d j element for 1st matrix : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            printf("Enter the number of %d i %d j element for 2nd matrix : ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            sum[i][j]=a[i][0]*b[0][j]+a[i][1]*b[1][j];
+            
+            // sum[0][0]=a[0][0]*b[0][0]+a[0][1]*b[1][0];
+            // sum[0][1]=a[0][0]*b[0][1]+a[0][1]*b[1][1];
+            // sum[1][0]=a[1][0]*b[0][0]+a[1][1]*b[1][0];
+            // sum[1][1]=a[1][0]*b[0][1]+a[1][1]*b[1][1];
+        }
+    }
+    printf("The product is : \n");
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            printf("%d\t",sum[i][j]);
+        }
+        printf("\n\n");
+    }
+}
