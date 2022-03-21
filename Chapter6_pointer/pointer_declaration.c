a516abaa8a21cc1cf4b179e71908085a899aa209
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-21 09:40:01 +0545

    Value from pointer

--- Chapter6_pointer/pointer_declaration.c
+++ Chapter6_pointer/pointer_declaration.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
     int *ptr_var;
-    int number =12345;
-    ptr_var=&number;
-    printf("The address of %d is : %p",number,ptr_var);
+    int number = 12345;
+    ptr_var = &number;
+    printf("The address of %d is : %p\n", number, ptr_var);
+    printf("The value using pointer is %d", *ptr_var);
 }
