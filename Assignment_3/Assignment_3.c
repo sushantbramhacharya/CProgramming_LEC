077dd5cbde4741e911d8beae29a6827ae4867c1e
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-03-22 08:52:19 +0545

    Tax Assignemnt 3

--- Assignment_3/Assignment_3.c
+++ Assignment_3/Assignment_3.c
@@ -1,14 +1,45 @@
-#include<stdio.h>
-int add(int a,int b,int c)
+#include <stdio.h>
+int add(int a, int b, int c)
 {
-    return a+b+c;
+    return a + b + c;
+}
+void tax(int salary[], int n)
+{
+    float tax[100];
+    for (int i = 0; i < n; i++)
+    {
+        if (salary[i] < 9000)
+        {
+            tax[i] = salary[i] * 0.2;
+        }
+        else
+        {
+            tax[i] = salary[i] * 0.25;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The tax of %dnd employee is %.2f \n", i + 1, tax[i]);
+    }
 }
 
 void main()
 {
-    int a,b,c,result;
-    printf("Enter the three numbers : ");
-    scanf("%d%d%d",&a,&b,&c);
-    result=add(a,b,c);
-    printf("%d",result);
+    // ADD
+    // int a,b,c,result;
+    // printf("Enter the three numbers : ");
+    // scanf("%d%d%d",&a,&b,&c);
+    // result=add(a,b,c);
+    // printf("%d",result);
+
+    // TAX
+    int salary[100], n;
+    printf("Enter the number of Employee : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the salary of %d nd employee : ", i + 1);
+        scanf("%d", &salary[i]);
+    }
+    tax(salary, n);
 }
