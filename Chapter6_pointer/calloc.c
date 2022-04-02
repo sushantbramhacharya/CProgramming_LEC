06b47749f97efb6efe7a639621390d9c876d8b90
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-04-02 08:50:20 +0545

    Calloc Rearranged

---
+++ Chapter6_pointer/calloc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr,n,sum=0;
+    printf("Enter the number you want to be allocated : ");
+    scanf("%d",&n);
+    ptr=(int*) calloc(n,sizeof(int));
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the number for %d index : ",i);
+        scanf("%d",(ptr+i));
+        sum=sum+*(ptr+i);
+    }
+    printf("The sum of numbers is %d.",sum);
+    free(ptr);
+}
