db68d0d43afbc4b609a16bfc4448fec02893caf9
sushantbramhacharya <sushantbramacharya@gmail.com>
2022-04-28 07:42:50 +0545

    Qn 2

---
+++ Lab_Practical_Sheet_1/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float a=22,b=7,sum,product,quotient;
+    sum=a+b;
+    product=a*b;
+    quotient=a/b;
+    printf("The sum is %f\nProduct is %f\nQuotient is %f",sum,product,quotient);
+}
